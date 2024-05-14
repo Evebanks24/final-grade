907ee21fea05641c6ebc6a37586686d7d27fdf8b
Evetta Banks <164821729+Evebanks24@users.noreply.github.com>
2024-05-13 20:57:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ This function should return a number (final grade). There are four types of fina
 
 function finalGrade (exam, projects) {
     if(exam > 90 || projects > 10) return 100;
-    if(exam > 75 & projects >= 5) return 90;
-    if(exam > 50 & projects >= 2) return 75;
+    if(exam > 75 && projects >= 5) return 90;
+    if(exam > 50 && projects >= 2) return 75;
     return 0;
   }
